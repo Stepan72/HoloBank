a9a9587885854352b0fc788d2be7626834849ec9
Stepan72 <st.nim@list.ru>
2024-08-09 05:59:52 +0500

    GetStarted component finished v1.7

--- components/GetStarted.tsx
+++ components/GetStarted.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { arrowUp } from "@/assets";
+import Image from "next/image";
 
 function GetStarted() {
-  return <div>GetStarted check</div>;
+  return (
+    <div className="flexCenter size-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer">
+      <div className="flexCenter flex-col bg-primary size-[100%] rounded-full">
+        <div className="flexStart flex-row">
+          <p className="font-poppins font-medium text-[18px] leading-[18px] mr-2">
+            <span className="text-gradient">Get</span>
+          </p>
+          <Image
+            src={arrowUp}
+            alt="arrow"
+            className="size-[23px] object-contain"
+          />
+        </div>
+        <p className="font-poppins font-medium text-[18px] leading-[18px]">
+          <span className="text-gradient">Started</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default GetStarted;
